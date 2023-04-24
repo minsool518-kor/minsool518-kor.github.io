f6584953071b4f0a0186f789d54dbce04adf0cd2
immigration2000 <minsool200057@gmail.com>
2023-04-24 20:31:43 +0900

    Updates

---
+++ component---src-pages-404-js-21e2769b2f517ed11eae.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{w2l6:function(e,t,n){"use strict";n.r(t);var r=n("q1tI"),a=n.n(r),l=n("Bl7J"),o=n("vrFN");t.default=function(){return a.a.createElement(l.a,null,a.a.createElement(o.a,{title:"404: Not found"}),a.a.createElement("h1",null,"Nothing to see here..."),a.a.createElement("p",null,"Here isn't there. Sorry."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-21e2769b2f517ed11eae.js.map
